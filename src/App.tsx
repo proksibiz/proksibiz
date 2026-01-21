aafaaf3022504abe9535d84de308187a44e5e872
Proksi Biz Solution <proksibiz@gmail.com>
2026-01-22 01:43:29 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/proksibiz">
+      <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || "/"}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
